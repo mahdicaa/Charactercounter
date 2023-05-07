4bc20730a248cef06cd1c5469e16577d26db56db
Mahdi Forootan <forotan1376@gmail.com>
2023-05-08 02:59:51 +0430

    Adding initial commit for third homework.

---
+++ script.js
@@ -0,0 +1,13 @@
+function CharCounter (Input_str) {
+    let counter = [...Input_str].reduce((CurrChar, PrevChar) => {
+        if (!CurrChar[PrevChar]) {
+            CurrChar[PrevChar] = 1
+            }
+            else {
+                CurrChar[PrevChar] = CurrChar[PrevChar] + 1;
+            }
+            // delete CurrChar[" "]
+            return counter;
+            }, {});
+    }
+console.log(CharCounter("Hello world"))
